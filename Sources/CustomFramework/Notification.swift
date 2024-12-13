4e1c8120f4a099323a287960685b61ad6e30e411
Hammad <hammad.ahmed@m3tech.com.pk>
2024-12-13 16:17:43 +0500

    First Commit

---
+++ Sources/CustomFramework/Notification.swift
@@ -0,0 +1,32 @@
+//
+//  Notification.swift
+//  LocalNotificationFrameWork
+//
+//  Created by Hammad on 12/12/2024.
+//
+
+import UIKit
+
+class Notification {
+    
+    func scheduleNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        
+        // Trigger notification after 1 second
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        // Create a unique identifier for the request
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // Add the notification request to the system
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error adding notification: \(error.localizedDescription)")
+            }
+        }
+    }
+
+}
